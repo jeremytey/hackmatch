b840759f984efc47ca875151da1cb33efafcd6bf
Jeremy Tey <jeremy8598@gmail.com>
2026-04-08 12:40:15 +0800

    feat: updated app.ts and schema.prisma

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Application, Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import cors from 'cors'; //
 import cookieParser from 'cookie-parser';
 import { logger } from './lib/logger'; // Winston logger
@@ -8,9 +8,8 @@ import { errorMiddleware } from './middleware/error.middleware';
 import authRouter from './routes/auth.routes';
 import userRouter from './routes/user.routes';
 import hackathonRouter from './routes/hackathon.routes';
-import participantRouter from './routes/participant.routes';
 
-const app:Application = express();
+const app = express();
 
 // Global Middleware
 app.use(cors({
@@ -20,19 +19,17 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(logger);
-app.use(errorMiddleware);
 
-// Custom Winston Request Logger Middleware
+// Custom Winston Request Logger Middleware - Logs HTTP method and URL for each incoming request
 app.use((req: Request, res: Response, next: NextFunction) => {
   logger.info(`${req.method} ${req.url}`);
   next();
 });
 
+// Routes
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/hackathons', hackathonRouter);
-app.use('/participants', participantRouter);
 
 app.use(errorMiddleware);
 
