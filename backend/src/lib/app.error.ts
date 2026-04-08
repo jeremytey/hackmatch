232c6a0f809be465d52c8eae57cce66c9f81e5bd
Jeremy Tey <jeremy8598@gmail.com>
2026-04-08 22:03:12 +0800

    feat: appError for status code

---
+++ backend/src/lib/app.error.ts
@@ -0,0 +1,14 @@
+// app.error.ts provides a custom error class for our application, allowing us to throw errors 
+// with specific status codes and messages. 
+
+export class AppError extends Error {
+  public readonly statusCode: number;
+  public readonly isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Object.setPrototypeOf(this, AppError.prototype);
+  }
+}
